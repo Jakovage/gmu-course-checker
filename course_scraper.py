9af492ffc53c7b577b1431c7d7acbaf7d96f3a21
Jakovage <jakovage@gmail.com>
2025-07-31 01:23:47 -0400

    replace get_crn() in course_scraper.py with get_listing_soup()
    given a course code and section, get_listing_soup() returns a the html of a course listing from the p_get_crse_unsec page.

--- course_scraper.py
+++ course_scraper.py
@@ -1,5 +1,6 @@
-from bs4 import BeautifulSoup
 from data import BASE_URL, ENDPOINTS, TERM_DATE_VALUES, CRSE_UNSC_VALUES
+from bs4 import BeautifulSoup
+from course import Course
 import requests
 import re
 
@@ -21,6 +22,7 @@ class CourseScraper:
 
         return valid_subjects
 
+    # returns the soup of the 2nd page, where you're inputting all the information of the class.
     def _get_term_date_soup(self, soup):
         form = soup.find("form")
         hidden_inputs = form.find_all("input", type='hidden')
@@ -31,6 +33,7 @@ class CourseScraper:
         post_response = self.session.post(post_url, data=data)
         return BeautifulSoup(post_response.text, "html.parser")
     
+    # returns the soup of the 3rd page of all the courses under a subject.
     def _get_crse_unsec_soup(self, soup, subject):
         valid_subjects = self._fetch_valid_subjects(soup) # list of all valid course subjects
         if subject not in valid_subjects:
@@ -49,6 +52,7 @@ class CourseScraper:
 
         return BeautifulSoup(post_response.text, "html.parser")
     
+    # scrapes from the initial term select page to the page containing all courses in a certain subject. returns that soup
     def _get_courses_page(self, subject):
         url = f"{BASE_URL}{ENDPOINTS["dyn_sched"]}"
 
@@ -68,16 +72,23 @@ class CourseScraper:
         soup = self._get_crse_unsec_soup(soup, subject) # fills out form for course serach page with valid data, posts to course listings, and return a soup.
 
         return soup # this soup represents
-                
-    def get_crn(self, subject, target_number, target_section):
+    
+    # returns the 3 tr elements in a course listing as html
+    def get_listing_soup(self, subject, target_number, target_section):
         soup = self._get_courses_page(subject)
 
         table = soup.find("table", {"class" : "datadisplaytable"})
-        rows = table.find_all("th", class_="ddtitle") # only html rows with class ddtitle contain the relevant information for extracting the CRN
+        #rows = table.find_all("th", class_="ddtitle") # only html rows with class ddtitle contain the relevant information for extracting the CRN
 
-        for row in rows:
+        rows = [
+            tr for tr in table.find_all("tr")
+            if tr.find_parent("tr") is None
+        ]
+        
+        for i, row in enumerate(rows[::3]):
             listing = row.find("a")
             listing_text = listing.get_text(strip=True)
+            #print(listing_text)
 
             # converts something like this: "Precalculus Mathematics - 71565 - MATH 105 - 001"
             # into this: ["Precalculus Mathematics", "71565", "MATH 105", "001"]
@@ -90,6 +101,12 @@ class CourseScraper:
 
                 # returns the CRN if the target number and course section are found
                 if course_number == target_number and course_section == target_section:
-                    return parts[-3]
-                
+                    #print(listing_text)
+                    new_html = rows[i*3].text + rows[(i*3) + 1].text + rows[(i*3) + 2].text
+                    return BeautifulSoup(new_html, "html.parser")
+        #'''
         return None
+
+    # returns a course object with its information populated by the html returned by get_listing()
+    def get_course(self, subject, target_number, target_section):
+        #soup = self.get_listing_soup(subject, target_number, target_section)
